458c4af88106fa6809ff1d75375b1dcd5dab8c30
pritam soni <pritam.soni13@gmail.com>
2022-05-10 01:40:53 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ import Discord from "./assets/Images/discord.png";
 import HomePage from "./pages/homePage/Index";
 import Crypto from './assets/Images/crypto.png';
 import Star from './assets/Images/star.png';
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
